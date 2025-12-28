6c01fb6e3350595451293ebae42f1a95b82770f9
ravikumar-919 <golusinghrathoure919@gmail.com>
2025-12-28 17:14:38 +0530

    Data Types Day1

---
+++ Day1.js
@@ -0,0 +1,43 @@
+//Data Type in Java Script
+/*
+ Number - limit upto 2 to power 53
+Bigint
+string - 
+boolean - true/false
+null - stand alone value
+undefined 
+symbol - used for unique
+object
+NaN- not a number
+*/
+
+// how to check datatype in js
+// let name = "tango"
+// console.log(typeof(name))
+
+// Type Casting 
+let age = "33"
+console.log(typeof age)
+let umar = Number(age)
+console.log(typeof umar)
+
+// ****************** Opreations in js ******************
+// arthmetic operators
+// + - * / % ++ --
+
+// assignment operators
+// = += -= *= /= %=
+
+// comparison operators
+// == === != !== > < >= <=
+
+// logical operators
+// && || !
+
+// bitwise operators
+// & | ^ ~ << >> >>>
+// ternary operators
+
+// condition ? true : false
+
+// ****************** Conditional Statements in js ******************   
